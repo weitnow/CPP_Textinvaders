af76ee03bfcc44b7e4fe9e1c5fd0af5b4fcb9bee
weitnow <weitnow@gmail.com>
2023-11-19 12:42:15 +0100

    second commit

---
+++ build/CMakeFiles/TextInvaders.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TextInvaders.dir/CursesUtils.cpp.o"
+  "CMakeFiles/TextInvaders.dir/CursesUtils.cpp.o.d"
+  "CMakeFiles/TextInvaders.dir/TextInvaders.cpp.o"
+  "CMakeFiles/TextInvaders.dir/TextInvaders.cpp.o.d"
+  "TextInvaders"
+  "TextInvaders.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TextInvaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
